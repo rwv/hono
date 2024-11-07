be3b2d01d0f3a12ba77a1a6e9f7c0c2bb38a75dd
Seedgou <i@zczc.cz>
2024-11-07 15:33:04 +0800

    test: add fc3 test

---
+++ aliyun-fc3-test/index.ts
@@ -0,0 +1,19 @@
+import { Hono } from '../src'
+import { handle } from '../src/adapter/aliyun-fc3'
+import type { AliyunFCContext, AliyunFCEvent } from '../src/adapter/aliyun-fc3'
+
+interface Env {
+  Bindings: {
+    event: AliyunFCEvent
+    context: AliyunFCContext
+  }
+}
+
+const app = new Hono<Env>()
+
+app.all('*', (c) => {
+  const { event, context } = c.env
+  return c.json({ event, context })
+})
+
+export default handle(app)
